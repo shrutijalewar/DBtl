f9899c28ed9853a0aff03003db6eee79fc8ec4ca
Shruti Sharma <shrutijalewar@gmail.com>
2014-11-20 14:48:45 -0600

    added auth frontEnd

--- client/nav.js
+++ client/nav.js
@@ -1,6 +1,12 @@
 (function(){
     'use strict';
 
-    angular.module('dbtl').controller('NavCtrl', ['$scope', '$state', function($scope, $state){
+    angular.module('dbtl')
+        .controller('NavCtrl', ['$scope', '$state', 'User', function($scope, $state, User){
+        $scope.logout = function(){
+            User.logout().then(function(){
+                toastr.success('User successfully logged out.');
+            });
+        };
     }]);
 })();
--- client/views/auth/auth.js
+++ client/views/auth/auth.js
@@ -2,6 +2,28 @@
     'use strict';
 
     angular.module('dbtl')
-        .controller('AuthCtrl', ['$scope', function($scope){
+        .controller('AuthCtrl', ['$scope', '$state', 'User',function($scope, $state, User){
+            $scope.user = {};
+            $scope.mode = $state.current.name;
+
+            $scope.submit = function(){
+                if($scope.mode === 'register'){
+                    User.register($scope.user).then(function(response){
+                        toastr.success('You have successfully registered.');
+                        $state.go('login');
+                    }, function(){
+                        toastr.error('Error during registration');
+                        $scope.user = {};
+                    });
+                }else{
+                    User.login($scope.user).then(function(response){
+                        toastr.success('You are logged in.');
+                        $state.go('home');
+                    }, function(){
+                        toastr.error('Error during login.');
+                        $scope.user = {};
+                    });
+                }
+            };
         }]);
 })();
