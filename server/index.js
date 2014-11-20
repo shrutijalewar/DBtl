3fd9afaca0d358b45d4bf868fb6f7ae8f054afad
Brian Hiatt <bchiatt>
2014-11-20 09:36:17 -0600

    clean up index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 'use strict';
 
 var Hapi       = require('hapi'),
-    server     = new Hapi.Server(process.env.PORT),
+    server     = new Hapi.Server('localhost', process.env.PORT),
     routes     = require('./routes/routes'),
     plugins    = require('./routes/plugins'),
     mongoose   = require('mongoose').connect(process.env.DB);
 
-server.route(routes);
 /*server.views({
     engines: {},
     basePath: __dirname,
@@ -18,8 +17,9 @@ server.route(routes);
 
 mongoose.connection.once('open', function(){
   server.pack.register(plugins, function(){
-      server.start(function(){
-          server.auth.strategy('simple', 'basic', {validateFunc: require('./lib/security')});
+    server.route(routes);
+    server.auth.strategy('simple', 'basic', {validateFunc: require('./lib/security')});
+    server.start(function(){
       server.log('info', 'Server running at: ' + server.info.uri);
     });
   });
