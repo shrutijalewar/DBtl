a2988158e3c343f821b31460a255312f93c23099
Brian Hiatt <bchiatt>
2014-11-21 11:08:11 -0600

    Link client factories with server routes

--- client/components/factories/message.js
+++ client/components/factories/message.js
@@ -5,7 +5,7 @@
         .factory('Message', ['$http', function($http){
 
             function send(message){
-                return $http.post('/message', message);
+                return $http.post('/messages', message);
             }
 
             return {send:send};
--- client/components/factories/search.js
+++ client/components/factories/search.js
@@ -5,15 +5,15 @@
         .factory('Search', ['$http', function($http){
 
             function create(search){
-                return $http.post('/message', search);
+                return $http.post('/searches', search);
             }
 
             function getById(id){
-                return $http.get('/message/' + id);
+                return $http.get('/search/' + id);
             }
 
             function remove(id){
-                return $http.delete('/message/' + id);
+                return $http.delete('/search/' + id);
             }
 
             return {create:create, getById:getById, remove:remove};
