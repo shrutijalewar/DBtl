c553f14cadc812dac09e1bdd5408b1430dfd501b
Shruti Sharma <shrutijalewar@gmail.com>
2014-11-22 17:59:21 -0600

    message in progress

--- client/views/message/message.js
+++ client/views/message/message.js
@@ -2,6 +2,17 @@
     'use strict';
 
     angular.module('dbtl')
-        .controller('MessageCtrl', ['$scope', function($scope){
+        .controller('MessageCtrl', ['$scope','User', 'Message', function($scope, User, Message){
+            $scope.sendMessage = function(){
+                var message = {
+                    toId: $scope.user._id,
+                    frId: $scope.user.username,
+                    pic: $scope.message.pic,
+                    body:$scope.message.body
+                };
+             User.send(message).then(function(response){
+                 toastr.success('Your message has been sent!');
+             });
+            };
         }]);
 })();
