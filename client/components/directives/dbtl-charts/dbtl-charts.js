dd8d9763ec1a4c75fbfb2dbab99f8b6ae72faeba
DSRoden <Daniel.S.Roden@gmail.com>
2014-11-23 09:55:44 -0600

    amcharts directive

--- client/components/directives/dbtl-charts/dbtl-charts.js
+++ client/components/directives/dbtl-charts/dbtl-charts.js
@@ -1,3 +1,5 @@
+/*global AmCharts */
+
 (function(){
   'use strict';
 
@@ -7,8 +9,62 @@
 
     o.restrict    = 'A';
     o.templateUrl = '/components/directives/dbtl-charts/dbtl-charts.html';
-    o.scope       = {xAxis:'@', yAxis:'@'};
+    o.scope       = {data:'='};
+         // o.link        = function(scope, element, attrs){
+         // };
 
+        o.controller  = ['$scope', function($scope){
+            console.log($scope.data);
+
+
+            var chart = false,
+                data = $scope.data.search,
+                cField = $scope.data.settings.categoryField,
+                vField = $scope.data.settings.valueField,
+                vAxes = $scope.data.settings.valueAxes,
+                graphInfo = $scope.data.settings.graphInfo,
+                chartTitle = $scope.data.settings.chartTitle,
+
+            initChart = function(){
+                if (chart){chart.destroy();}
+                //var config = $scope.config || {};
+                chart = AmCharts.makeChart('chartdiv', {
+                    'type': 'serial',
+                    'categoryField': cField,
+                    'categoryAxis': {
+                        'gridPosition': 'start'
+                    },
+                    'graphs': [
+                        {
+                            'title': graphInfo,
+                            'valueField': vField,
+                            'type': 'line',
+                            'fillAlphas' : 0.5,
+                            'bullet' : 'round',
+                            'bulletAlpha': 1
+                        }
+                    ],
+                    'valueAxes': [
+                        {
+                            'title': vAxes
+                        }
+                    ],
+                    'legend': {
+                        'useGraphSettings': true
+                    },
+                    'titles': [
+                        {
+                            'size': 15,
+                            'text': chartTitle
+                        }
+                    ],
+                    'dataProvider': data
+                });
+
+
+            };
+            initChart();
+        }];
     return o;
   }]);
 })();
--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -3,6 +3,38 @@
 
     angular.module('dbtl')
         .controller('DashCtrl', ['$scope', function($scope){
+            $scope.object = {};
+            $scope.object2 = {};
+
+            $scope.object.settings= {categoryField: 'seed', valueField: 'images', valueAxes: 'Number of Images', graphInfo: 'Images Per Seed', chartTitle: 'Images/Seed'};
+            $scope.object.search = [{
+                'seed': 'www.cnn.com',
+                'images': 4252
+            }, {
+                'seed': 'www.fox.com',
+                'images': 1882
+            }, {
+                'seed': 'www.vox.com',
+                'images': 1809
+            }];
+
+                $scope.object2.settings = {
+                    categoryField: 'url',
+                    valueField: 'images',
+                    valueAxes: 'Number of Images',
+                    graphInfo: 'Images Per Url',
+                    chartTitle: 'Crawler'
+                };
+                $scope.object2.search = [{
+                    'url': 'www.cnn.com',
+                    'images': 4252
+                }, {
+                    'url': 'www.fox.com',
+                    'images': 1882
+                }, {
+                    'url': 'www.vox.com',
+                    'images': 1809
+                }];
         }]);
 })();
 
