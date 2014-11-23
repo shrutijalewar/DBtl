93f07b863b8c70706bbe25c74888bfffacf5f28d
DSRoden <Daniel.S.Roden@gmail.com>
2014-11-23 17:02:22 -0600

    amcharts directive revised

--- client/components/directives/dbtl-charts/dbtl-charts.js
+++ client/components/directives/dbtl-charts/dbtl-charts.js
@@ -10,25 +10,41 @@
     o.restrict    = 'A';
     o.templateUrl = '/components/directives/dbtl-charts/dbtl-charts.html';
     o.scope       = {data:'='};
-         // o.link        = function(scope, element, attrs){
-         // };
+          o.link        = function(scope, element, attrs){
+
+          };
 
         o.controller  = ['$scope', function($scope){
-            console.log($scope.data);
 
 
-            var chart = false,
-                data = $scope.data.search,
-                cField = $scope.data.settings.categoryField,
-                vField = $scope.data.settings.valueField,
-                vAxes = $scope.data.settings.valueAxes,
-                graphInfo = $scope.data.settings.graphInfo,
-                chartTitle = $scope.data.settings.chartTitle,
+            $scope.initChart = function(data){
 
-            initChart = function(){
-                if (chart){chart.destroy();}
+                if(data.moreThanOne){
+                    $scope.chart2Exists = true;
+                }
+
+                var chart1 = false,
+                cData = $scope.data.chart1Search,
+                cField = $scope.data.chart1Settings.categoryField,
+                vField = $scope.data.chart1Settings.valueField,
+                vAxes = $scope.data.chart1Settings.valueAxes,
+                graphInfo = $scope.data.chart1Settings.graphInfo,
+                chartTitle = $scope.data.chart1Settings.chartTitle,
+                chartNum = $scope.data.chart1Settings.chartNumber,
+            //chart 2
+                chart2 = false,
+                cData2 = $scope.data.chart2Search,
+                cField2 = $scope.data.chart2Settings.categoryField,
+                vField2 = $scope.data.chart2Settings.valueField,
+                vAxes2 = $scope.data.chart2Settings.valueAxes,
+                graphInfo2 = $scope.data.chart2Settings.graphInfo,
+                chartTitle2 = $scope.data.chart2Settings.chartTitle,
+                chartNum2 = $scope.data.chart2Settings.chartNumber;
                 //var config = $scope.config || {};
-                chart = AmCharts.makeChart('chartdiv', {
+                //var div = 'chart'+chartNum;
+
+                chart1 = AmCharts.makeChart('chartdiv1', {
+
                     'type': 'serial',
                     'categoryField': cField,
                     'categoryAxis': {
@@ -58,12 +74,54 @@
                             'text': chartTitle
                         }
                     ],
-                    'dataProvider': data
+                    'dataProvider': cData
                 });
+                console.log(chartNum);
 
+                chart2 = AmCharts.makeChart('chartdiv2', {
+
+                    'type': 'serial',
+                    'categoryField': cField2,
+                    'categoryAxis': {
+                        'gridPosition': 'start'
+                    },
+                    'graphs': [
+                        {
+                            'title': graphInfo2,
+                            'valueField': vField2,
+                            'type': 'line',
+                            'fillAlphas' : 0.5,
+                            'bullet' : 'round',
+                            'bulletAlpha': 1
+                        }
+                    ],
+                    'valueAxes': [
+                        {
+                            'title': vAxes2
+                        }
+                    ],
+                    'legend': {
+                        'useGraphSettings': true
+                    },
+                    'titles': [
+                        {
+                            'size': 15,
+                            'text': chartTitle2
+                        }
+                    ],
+                    'dataProvider': cData2
+                });
+            console.log(chartNum2);
 
             };
-            initChart();
+            //$scope.initChart();
+            $scope.$watch('data', function(newValue, oldValue){
+                if (newValue) {
+
+                    $scope.initChart(newValue);
+                    console.log(newValue, oldValue);
+                }
+            }, true);
         }];
     return o;
   }]);
--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -3,11 +3,24 @@
 
     angular.module('dbtl')
         .controller('DashCtrl', ['$scope', function($scope){
-            $scope.object = {};
-            $scope.object2 = {};
 
-            $scope.object.settings= {categoryField: 'seed', valueField: 'images', valueAxes: 'Number of Images', graphInfo: 'Images Per Seed', chartTitle: 'Images/Seed'};
-            $scope.object.search = [{
+            //CHARTS//
+
+            //initialize//
+            $scope.chartsObject = {};
+            $scope.chartsObject.moreThanOne = false;
+
+
+
+            //create chart objects
+            $scope.chartsObject.chart1Settings= {categoryField: 'seed',
+                valueField: 'images',
+                valueAxes: 'Number of Images',
+                graphInfo: 'Images Per Seed',
+                chartTitle: 'Images/Seed',
+                chartNumber: 'chartdiv'};
+
+            $scope.chartsObject.chart1Search = [{
                 'seed': 'www.cnn.com',
                 'images': 4252
             }, {
@@ -18,23 +31,34 @@
                 'images': 1809
             }];
 
-                $scope.object2.settings = {
-                    categoryField: 'url',
-                    valueField: 'images',
-                    valueAxes: 'Number of Images',
-                    graphInfo: 'Images Per Url',
-                    chartTitle: 'Crawler'
-                };
-                $scope.object2.search = [{
-                    'url': 'www.cnn.com',
-                    'images': 4252
-                }, {
-                    'url': 'www.fox.com',
-                    'images': 1882
-                }, {
-                    'url': 'www.vox.com',
-                    'images': 1809
-                }];
+            $scope.chartsObject.chart2Settings = {
+                categoryField: 'url',
+                valueField: 'images',
+                valueAxes: 'Number of Images',
+                graphInfo: 'Images Per Url',
+                chartTitle: 'Crawler',
+                chartNumber: '#chartdiv2'
+            };
+            $scope.chartsObject.chart2Search = [{
+                'url': 'www.cnn.com',
+                'images': 4252
+            }, {
+                'url': 'www.fox.com',
+                'images': 1882
+            }, {
+                'url': 'www.vox.com',
+                'images': 1809
+            }];
+
+            //function to show second chart//
+
+            $scope.showCrawlerChart = function(){
+                $scope.chartsObject.moreThanOne=true;
+                console.log('trying to show second chart');
+                $scope.chartsObject.chart1Settings.chartTitle='changing';
+                var print = $scope.chartsObject.chart1Settings.chartTitle;
+                console.log(print);
+            };
         }]);
 })();
 
